f8e84cac7bb030a3977e3a37437d661e7d0806d6
wy963971867 <zh_le_le@163.com>
2016-01-07 10:21:40 +0800

    提交到本地仓库

---
+++ src/com/test/SummitGit.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class SummitGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("第二次提交");
+	}
+
+}
